e7802900e4144ae1f83f3db312bf9976e37ee845
Dimpin <you@example.comwdm.dimpin@gmail.com>
2025-05-06 14:09:20 +0500

    update

--- backend/prisma/generated/client/edge.js
+++ backend/prisma/generated/client/edge.js
@@ -554,7 +554,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
--- backend/prisma/generated/client/index.js
+++ backend/prisma/generated/client/index.js
@@ -555,7 +555,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
