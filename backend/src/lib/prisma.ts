346850dc8ccce09ef3b2211a019a19cd7d7795f8
Dimpin <you@example.comwdm.dimpin@gmail.com>
2025-05-07 09:03:54 +0500

    fix: use dynamic prisma client for dev and prod

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -2,8 +2,21 @@
 
 import { resolve } from 'path'
 
+// Указываем, где искать клиент при запуске через node dist/server.js
 process.env.PRISMA_CLIENT_LOCATION = resolve(__dirname, '../prisma/client')
 
-import { PrismaClient } from '@prisma/client'
+// Локально используем @prisma/client
+// В проде используем ../../../prisma/client (после билда)
+let PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  // В проде клиент находится в dist/prisma/client
+  const { PrismaClient: ProdPrismaClient } = require('../../../prisma/client')
+  PrismaClient = ProdPrismaClient
+} else {
+  // В деве используем стандартный путь
+  const { PrismaClient: DevPrismaClient } = require('@prisma/client')
+  PrismaClient = DevPrismaClient
+}
 
 export const prisma = new PrismaClient()
