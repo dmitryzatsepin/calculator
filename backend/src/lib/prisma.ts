f0e16c0ee6e94228b49d59b1a451fb681a9111dd
Dimpin <you@example.comwdm.dimpin@gmail.com>
2025-05-07 09:17:43 +0500

    fix: use local path to prisma client in production

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,9 +1,9 @@
 import { resolve } from 'path'
 
-// Эта строка должна быть первой!
+// Указываем, где искать клиент
 process.env.PRISMA_CLIENT_LOCATION = resolve(__dirname, '../prisma/client')
 
-// Теперь импортируем клиент
-const { PrismaClient } = require('@prisma/client')
+// Теперь импортируем клиент напрямую из dist
+const { PrismaClient } = require('../prisma/client')
 
 export const prisma = new PrismaClient()
